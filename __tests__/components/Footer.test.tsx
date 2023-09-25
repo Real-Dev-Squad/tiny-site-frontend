be6c16e916e01fff52fd12bdfcf7d569867d8a86
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-26 04:57:26 +0530

    TEST: add test for Footer, Navbar and ProfileIcon

---
+++ __tests__/components/Footer.test.tsx
@@ -0,0 +1,19 @@
+import { Footer } from '@/components/Footer/Footer';
+import { render } from '@testing-library/react';
+
+describe('Footer', () => {
+    it('should render', () => {
+        const { container } = render(<Footer />);
+        expect(container).toHaveTextContent('The contents of this website are deployed from this open sourced repo');
+    });
+
+    it('should have a link to the repo', () => {
+        const { container } = render(<Footer />);
+        expect(container.querySelector('a')).toHaveAttribute('href', '#');
+    });
+
+    it('should have a link to the repo', () => {
+        const { container } = render(<Footer />);
+        expect(container.querySelector('a')).toHaveAttribute('target', '_blank');
+    });
+});
--- src/components/ProfileIcon/ProfileIcon.tsx
+++ src/components/ProfileIcon/ProfileIcon.tsx
@@ -1,4 +1,3 @@
-// components/ProfileIcon.tsx
 import React from 'react';
 
 interface ProfileIconProps {
