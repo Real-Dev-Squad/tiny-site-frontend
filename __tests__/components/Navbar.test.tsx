0eae4092f7f11948a2df3558b5b5cfa583b87c0e
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-29 00:34:39 +0530

    FEATURE: Landing Page (#11)
    * FEATURE: add code changes for landing page
    
    * FEAT: redesign dashboard
    
    * FEAT: add changes to generate fake short URL
    
    * FEAT: Add component to generate profile icon
    
    * TEST: add test for Footer, Navbar and ProfileIcon
    
    * WORKFLOW: add command to run in workflow
    
    * WORKFLOW: add command to run in workflow

---
+++ __tests__/components/Navbar.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar } from '@/components/Navbar/Navbar';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Navbar', () => {
+    it('should render', () => {
+        const { container } = render(<Navbar />);
+        expect(container).toHaveTextContent('URL Shortener');
+        expect(container.querySelector('a')).toHaveAttribute('href', '#');
+    });
+
+    it('should have dropdown menu', () => {
+        const { container } = render(<Navbar />);
+        expect(container.querySelector('ul')).toHaveTextContent('Profile');
+        expect(container.querySelector('ul')).toHaveTextContent('Dashboard');
+        expect(container.querySelector('ul')).toHaveTextContent('Settings');
+    });
+
+    it('should toggle menu', () => {
+        const { container } = render(<Navbar />);
+        const button = container.querySelector('button');
+        expect(button).toHaveTextContent('Sunny');
+        button?.click();
+        expect(container.querySelector('ul')).toHaveClass('lg:flex space-x-4');
+    });
+});
