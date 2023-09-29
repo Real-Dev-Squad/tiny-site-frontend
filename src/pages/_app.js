f587e64e10a312f5ec2f476b9b547f6fc9f40ecd
GAURAV SINHA <sinhagaurav.me@gmail.com>
2023-09-30 00:03:13 +0530

    feat: login page (#17)
    * feat: login page(UI)
    
    * removed comments

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/global.css';
+import { Footer } from '@/components/Footer/Footer';
+import { Navbar } from '@/components/Navbar/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <title>URL Shortener</title>
+            <Navbar />
+            <div className="bg-gray-900 flex flex-col justify-center items-center h-container">
+                <Component {...pageProps} />
+            </div>
+            <Footer />
+        </div>
+    );
 }
