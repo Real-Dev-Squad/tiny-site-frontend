87c71726fac3319b1a1f9aad06cc0482404787b1
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-04 20:47:37 +0530

    add backend api url for testing

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
-export const TINY_API_URL = 'http://localhost:8000/v1';
+export const TINY_API_URL = 'https://tiny-site-backend.onrender.com/v1';
 export const TINY_API_GOOGLE_LOGIN = `${TINY_API_URL}/auth/google/login`;
 export const TINY_API_LOGOUT = `${TINY_API_URL}/auth/logout`;
