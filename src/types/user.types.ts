15a1d23de319e8bda3bea37bce6886a3e31d236e
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-06 10:48:00 +0530

    Integration: With backend for sign and signout (#34)
    * package: installed necessary packages for testing
    
    * test: add test for changes
    
    * assets: added google icon, url constants and user type
    
    * hooks: added custom IsAuthenticated hooks for login
    
    * add backend api url for testing
    
    * refactor: create separate svg component for DownArrow icon, create separate directory for fixtures and change API url

---
+++ src/types/user.types.ts
@@ -0,0 +1,10 @@
+export interface UserTypes {
+    id: number;
+    Username: string;
+    email: string;
+    password: string;
+    isVerified: boolean;
+    isOnboarding: boolean;
+    createdAt: string;
+    updatedAt: string;
+}
