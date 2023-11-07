8e5a52830620405dd47289a07814ccabc8121453
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-07 13:25:49 +0530

    config: add changes to not generate snapshots

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
+    snapshotSerializers: [],
 };
 
 module.exports = createJestConfig(customJestConfig);
