48cff5a03bd7c50664a6bff501c36bcc1c6bd81f
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-07 20:07:32 +0530

    fix: change backend API URL

--- __tests__/components/Navbar.test.tsx
+++ __tests__/components/Navbar.test.tsx
@@ -22,7 +22,10 @@ describe('Navbar', () => {
         const googleLoginButton = screen.getByTestId('google-login');
         expect(googleLoginButton).toBeInTheDocument();
         expect(googleLoginButton).toHaveTextContent('Sign In');
-        expect(googleLoginButton).toHaveAttribute('href', 'https://api-tinysite.onrender.com/v1/auth/google/login');
+        expect(googleLoginButton).toHaveAttribute(
+            'href',
+            'https://staging-tinysite-api.realdevsquad.com/v1/auth/google/login'
+        );
     });
 
     it('should display "Sign In" when not logged in', () => {
