6d6ab9c3bf921b917c945daf2e69a2a0ffd7f61c
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-09 23:09:58 +0530

    feat: add functionality for redirect ot original url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/:shortCode',
+                destination: '/redirect/:shortCode',
+                permanent: false,
+            },
+        ];
+    },
+};
 
 module.exports = nextConfig;
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,3 +2,5 @@ export const TINY_API_URL = 'https://staging-tinysite-api.realdevsquad.com/v1';
 export const TINY_API_GOOGLE_LOGIN = `${TINY_API_URL}/auth/google/login`;
 export const TINY_API_LOGOUT = `${TINY_API_URL}/auth/logout`;
 export const BASE_SHORT_URL = 'https://staging-tinysite.realdevsquad.com';
+export const TINY_API_URL_DETAIL = `${TINY_API_URL}/urls`;
+export const TINY_API_REDIRECT = `${TINY_API_URL}/tinyurl`;
