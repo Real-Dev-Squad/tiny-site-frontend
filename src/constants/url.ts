deb037d12b554f18d05e146ffde9d2eee89a3ab6
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-10 13:06:53 +0530

    setup: add package and env folr for different environment like dev, staging and prod

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,4 +1,6 @@
-export const TINY_API_URL = 'https://staging-tinysite-api.realdevsquad.com/v1';
-export const TINY_API_GOOGLE_LOGIN = `${TINY_API_URL}/auth/google/login`;
-export const TINY_API_LOGOUT = `${TINY_API_URL}/auth/logout`;
-export const BASE_SHORT_URL = 'https://staging-tinysite.realdevsquad.com';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://staging-tinysite-api.realdevsquad.com/v1';
+export const BASE_SHORT_URL = process.env.NEXT_PUBLIC_BASE_SHORT_URL || 'https://staging-tinysite.realdevsquad.com';
+
+export const TINY_API_URL = API_BASE_URL;
+export const TINY_API_GOOGLE_LOGIN = `${API_BASE_URL}/auth/google/login`;
+export const TINY_API_LOGOUT = `${API_BASE_URL}/auth/logout`;
