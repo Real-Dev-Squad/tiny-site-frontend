0c372c6aa39f1b28ce4de49ef8b58a411221bc2b
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-11 08:39:55 +0530

    refactor and fix test cases

--- src/pages/[redirect]/index.tsx
+++ src/pages/[redirect]/index.tsx
@@ -1,9 +1,50 @@
-import fetchOriginalUrl from '@/utils/fetchOriginalUrl';
-import RedirectIcon from '../../../public/assets/icons/redirect';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 import Head from 'next/head';
+import RedirectIcon from '../../../public/assets/icons/redirect';
 import { TINY_SITE } from '@/constants/url';
+import fetchOriginalUrl from '@/utils/fetchOriginalUrl';
+import { useRouter } from 'next/router';
+
+interface LoaderTimerProps {
+    timer: number;
+    goButtonClickHandler: () => void;
+}
+
+const LoaderTimer = ({ timer, goButtonClickHandler }: LoaderTimerProps) => {
+    if (timer < 1) {
+        return (
+            <div className="mt-4 flex flex-col items-center space-y-2">
+                <RedirectIcon />
+                <p className="text-1xl">Redirecting...</p>
+            </div>
+        );
+    } else {
+        return (
+            <>
+                <div className="loader border-t-4 rounded-full border-gray-500 animate-spin aspect-square w-20 flex justify-center items-center text-yellow-700 text-4xl font-bold mt-4">
+                    {timer}
+                </div>
+                <button onClick={goButtonClickHandler} className="mt-4 p-2 bg-blue-500 text-white rounded-md">
+                    Go
+                </button>
+            </>
+        );
+    }
+};
+
+const RedirectFooter = () => (
+    <div className="absolute bottom-0 right-0 p-2 text-gray-500 w-screen flex justify-center items-center">
+        <a
+            className="text-sm text-gray-400 font-bold cursor-pointer hover:underline"
+            href={TINY_SITE}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            By <span className="font-bold">Real Dev Squad</span>
+        </a>
+    </div>
+);
 
 const Redirect = () => {
     const router = useRouter();
@@ -17,18 +58,19 @@ const Redirect = () => {
         setIsPremiumUser(false);
 
         if (shortUrlCode) {
-            fetchOriginalUrl(shortUrlCode)
-                .then((url) => {
+            const fetchOriginalUrlAsync = async () => {
+                try {
+                    const url = await fetchOriginalUrl(shortUrlCode);
                     if (url) {
                         setOriginalUrl(url);
                     } else {
-                        console.log('Short URL code not found');
                         router.push('/');
                     }
-                })
-                .catch((error) => {
+                } catch (error) {
                     console.error('Error fetching original URL:', error);
-                });
+                }
+            };
+            fetchOriginalUrlAsync();
         }
 
         if (timer > 0) {
@@ -56,38 +98,14 @@ const Redirect = () => {
             <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-4">
                 <p className="text-lg">You are being redirected to:</p>
                 <p className="text-blue-500 text-xl font-bold w-1/2 text-center truncate xl:w-1/2">{originalUrl}</p>
-                {timer < 1 ? (
-                    <div className="mt-4 flex flex-col items-center space-y-2">
-                        <RedirectIcon />
-                        <p className="text-1xl">Redirecting...</p>
-                    </div>
-                ) : (
-                    <>
-                        <div className="loader border-t-4 rounded-full border-gray-500 animate-spin aspect-square w-20 flex justify-center items-center text-yellow-700 text-4xl font-bold mt-4">
-                            {timer}
-                        </div>
-                        <button onClick={handleGoButtonClick} className="mt-4 p-2 bg-blue-500 text-white rounded-md">
-                            Go
-                        </button>
-                    </>
-                )}
+                <LoaderTimer timer={timer} goButtonClickHandler={handleGoButtonClick} />
 
                 {showTooltip && !isPremiumUser && (
                     <div className="mt-2 p-2 bg-yellow-100 text-yellow-800 rounded-md">
                         The skip feature is exclusively available to Premium users.
                     </div>
                 )}
-
-                <div className="absolute bottom-0 right-0 p-2 text-gray-500 w-screen flex justify-center items-center">
-                    <a
-                        className="text-sm text-gray-400 font-bold cursor-pointer hover:underline"
-                        href={TINY_SITE}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By <span className="font-bold">Real Dev Squad</span>
-                    </a>
-                </div>
+                <RedirectFooter />
             </div>
         </>
     );
