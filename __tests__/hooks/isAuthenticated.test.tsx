b45de5e62d50047ab8ab6d65d9b8fe100add33ce
Vinit <vinit224488@gmail.com>
2023-11-19 11:33:27 +0530

    add more test for isauthenticated hook

--- __tests__/hooks/isAuthenticated.test.tsx
+++ __tests__/hooks/isAuthenticated.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import axios from 'axios';
 
 import IsAuthenticated from '@/hooks/isAuthenticated';
@@ -7,14 +7,40 @@ import { userData } from '../../fixtures/users';
 
 jest.mock('axios');
 
-jest.mock('axios');
-
 describe('IsAuthenticated', () => {
-    it('fetches successfully data from an API', async () => {
-        (axios.get as jest.Mock).mockResolvedValue({ status: 200, data: { data: userData } });
-        const { result, waitForNextUpdate } = renderHook(() => IsAuthenticated());
-        await waitForNextUpdate();
+    it('fetches successfully data from an API and sets state', async () => {
+        jest.spyOn(axios, 'get').mockResolvedValue({ status: 200, data: { data: userData } });
+        const { result, waitFor } = renderHook(() => IsAuthenticated());
+
+        await act(async () => {
+            await waitFor(() => result.current.isLoggedIn === true);
+        });
+
         expect(result.current.isLoggedIn).toBe(true);
         expect(result.current.userData).toEqual(userData);
+    });
+
+    it('handles unsuccessful API response and sets isLoggedIn to false', async () => {
+        jest.spyOn(axios, 'get').mockResolvedValue({ status: 401 });
+        const { result, waitFor } = renderHook(() => IsAuthenticated());
+
+        await act(async () => {
+            await waitFor(() => result.current.isLoggedIn === false);
+        });
+
+        expect(result.current.isLoggedIn).toBe(false);
+        expect(result.current.userData).toBe(null);
+    });
+
+    it('handles API error and sets isLoggedIn to false', async () => {
+        jest.spyOn(axios, 'get').mockRejectedValue(new Error('API Error'));
+        const { result, waitFor } = renderHook(() => IsAuthenticated());
+
+        await act(async () => {
+            await waitFor(() => result.current.isLoggedIn === false);
+        });
+
+        expect(result.current.isLoggedIn).toBe(false);
+        expect(result.current.userData).toBe(null);
     });
 });
