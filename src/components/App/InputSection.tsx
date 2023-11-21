b8ed0585c2753e082019332ec33d70795c4cdb52
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-21 16:04:25 +0530

    refact: remove icons and refactor auth hook and add shimmers (#63)
    * refact: remove isons and refactor auth hook and add shimmers
    
    * refactor: removed uncessary tailwind classes
    
    * Merge branch 'develop' of https://github.com/Real-Dev-Squad/tiny-site-frontend into refactor/hooks-icons
    
    * refactor: navbar component
    
    * feature: shimmer added in dashboardpage

--- src/components/App/InputSection.tsx
+++ src/components/App/InputSection.tsx
@@ -1,7 +1,6 @@
-import { ChangeEvent, FormEvent } from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
 
 import Button from '@/components/Button';
-import InputBox from '@/components/InputBox';
 
 interface InputSectionProps {
     url: string;
@@ -11,32 +10,33 @@ interface InputSectionProps {
 
 const InputSection: React.FC<InputSectionProps> = ({ url, setUrl, handleUrl }) => (
     <form
-        className="flex flex-col justify-center items-center rounded-2xl mt-5 sm:mt-10 w-[100%] text-gray-400"
+        className="flex flex-col items-center rounded-2xl mt-5 sm:mt-10 w-full text-gray-400"
         onSubmit={(e: FormEvent) => {
             e.preventDefault();
             handleUrl();
         }}
     >
-        <h1 className="text-2xl md:text-3xl xl:text-3xl text-center mb-4 text-white font-semibold">
+        <h1 className="text-3xl md:text-4xl xl:text-4xl text-center mb-4 text-white font-semibold">
             Enter a URL to shorten
         </h1>
 
-        <div className="bg-gray-200 flex flex-row justify-center items-center space-y-0 space-x-0 rounded-2xl mt-5 sm:mt-10 w-[100%]">
-            <label htmlFor="url-input" className="ml-2">
-                🔗
-            </label>
-            <InputBox
-                type="text"
-                hideLabel={true}
-                className="bg-gray-200 text-black outline-none p-4 rounded-l-2xl w-full  "
-                onChange={(e: ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)}
-                value={url}
-                placeholder="Enter the URL"
-                name="URL"
-            />
+        <div className="flex flex-col sm:flex-row items-center justify-center mt-5 sm:mt-10 w-full">
+            <div className="bg-gray-200 flex items-center rounded-2xl w-full  sm:w-2/4">
+                <label htmlFor="url-input" className="ml-2">
+                    🔗
+                </label>
+                <input
+                    type="text"
+                    className="bg-gray-200 text-black p-4 rounded-2xl focus:outline-none w-full"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)}
+                    value={url}
+                    placeholder="Enter the URL"
+                    name="URL"
+                />
+            </div>
             <Button
                 type="submit"
-                className="bg-gray-300 text-black text-lg rounded-r-2xl py-4 px-8 hover:bg-gray-400"
+                className="bg-gray-300 text-black text-lg rounded-2xl py-2 sm:py-4 px-8 hover:bg-gray-400 mt-2 sm:mt-0 sm:ml-2"
                 testId="shorten-button"
                 onClick={handleUrl}
             >
---
+++ src/components/ShimmerEffect/UserLoginShimmer.tsx
@@ -0,0 +1,10 @@
+const UserLoginShimmer = () => {
+    return (
+        <div className="flex items-center space-x-2 animate-pulse">
+            <div className="w-[46px] h-[46px] rounded-[50%] bg-gradient-to-r from-[#384B6B] via-[#526E8E] to-[#384B6B] flex items-center justify-center text-white text-lg p-4"></div>
+            <span className="bg-gradient-to-r from-[#526E8E] via-[#384B6B] to-[#526E8E] h-5 w-20 inline-block"></span>
+        </div>
+    );
+};
+
+export default UserLoginShimmer;
