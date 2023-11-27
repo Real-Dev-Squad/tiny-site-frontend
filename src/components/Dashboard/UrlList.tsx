5fb56015e6556215d008efda3755b5e9c76a5d36
Sunny Sahsi <sahsisunny@gmail.com>
2023-11-27 21:40:25 +0530

    refactor: dashboard page and break into different component

---
+++ src/components/Dashboard/UrlList.tsx
@@ -0,0 +1,24 @@
+import NoUrlFound from '@/components/Dashboard/NoUrlFound';
+import UrlListItem from '@/components/Dashboard/UrlListItem';
+import { UrlType } from '@/types/url.types';
+
+interface UrlListProps {
+    urls: UrlType[];
+    copyButtonHandler: (url: string) => void;
+}
+
+const UrlList = ({ urls, copyButtonHandler }: UrlListProps) => {
+    if (!urls.length) {
+        return <NoUrlFound />;
+    }
+    return (
+        <ul className="flex flex-col justify-center items-center w-full mt-10">
+            <h1 className="text-3xl md:text-4xl xl:text-4xl text-center mb-8 text-white font-semibold">Your URLs</h1>
+            {urls.map((url) => (
+                <UrlListItem key={url.shortUrl} url={url} copyButtonHandler={copyButtonHandler} />
+            ))}
+        </ul>
+    );
+};
+
+export default UrlList;
