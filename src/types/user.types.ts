d36ee92c2350a5f2def53a30aac768b6423d2d84
vinit <vinit224488@gmail.com>
2023-11-29 21:32:21 +0530

    refactor: user interface

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,10 +1,13 @@
-export interface UserTypes {
-    id: number;
-    userName: string;
-    email?: string;
-    password?: string;
-    isVerified?: boolean;
-    isOnboarding?: boolean;
-    createdAt?: string;
-    updatedAt?: string;
+export interface User {
+    message: string,
+    data: {
+        id: number;
+        userName: string;
+        email?: string;
+        password?: string;
+        isVerified?: boolean;
+        isOnboarding?: boolean;
+        createdAt?: string;
+        updatedAt?: string;
+    }
 }
