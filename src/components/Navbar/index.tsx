75842dcb7cd6fead132e0e5640db8e261a0118aa
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-11 16:27:02 +0530

    refactor: change logo name

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             <nav className="bg-gray-900 p-4 h-[8vh]">
                 <div className="flex items-center justify-between">
                     <Link href="/" className="flex">
-                        <Image src="/rds.png" alt="logo" width={30} height={30} className="mr-2 w-auto" />
+                        <Image src="/rdsli.png" alt="logo" width={30} height={30} className="mr-2 w-auto" />
                         <span className="text-white text-2xl font-bold">rds.li</span>
                     </Link>
 
