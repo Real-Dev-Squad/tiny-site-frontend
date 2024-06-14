b66efdfed9a8a6de977715148865607fffce0ddc
Yash Raj <56453897+yesyash@users.noreply.github.com>
2024-06-15 04:28:46 +0530

    add remaining count on dashboard & fix css (#111)

--- src/components/Dashboard/UrlList.tsx
+++ src/components/Dashboard/UrlList.tsx
@@ -6,14 +6,27 @@ interface UrlListProps {
     copyButtonHandler: (url: string) => void;
 }
 
+const MAX_URLS = 50;
+
 const UrlList = ({ urls, copyButtonHandler }: UrlListProps) => {
+    const remainingUrls = MAX_URLS - urls.length;
+
     return (
-        <ul className="flex flex-col justify-center items-center w-full mt-10">
-            <h1 className="text-3xl md:text-4xl xl:text-4xl text-center mb-8 text-white font-semibold">Your URLs</h1>
-            {urls.map((url) => (
-                <UrlListItem key={url.shortUrl} url={url} copyButtonHandler={copyButtonHandler} />
-            ))}
-        </ul>
+        <div className="w-full mt-10 flex flex-col justify-center items-center max-w-3xl mx-auto">
+            <div className="flex items-center pb-8 w-full justify-between">
+                <h1 className="text-3xl md:text-4xl xl:text-4xl text-center text-white font-semibold">Your URLs</h1>
+
+                <h4>
+                    Remaining: {remainingUrls} / {MAX_URLS}
+                </h4>
+            </div>
+
+            <ul className="w-full space-y-3">
+                {urls.map((url) => (
+                    <UrlListItem key={url.shortUrl} url={url} copyButtonHandler={copyButtonHandler} />
+                ))}
+            </ul>
+        </div>
     );
 };
 
