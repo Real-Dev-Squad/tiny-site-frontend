dba6512723412889c621b82e299d3cce8cb1a8f2
vinit717 <vinit224488@gmail.com>
2024-07-29 13:53:42 +0530

    ui: fix home page heading and input box

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
             },
+            fontSize: {
+                '80px': '80px',
+            },
+            lineHeight: {
+                'custom-120px': '100px',
+                'custom-60px': '60px',
+            },
         },
     },
     plugins: [],
