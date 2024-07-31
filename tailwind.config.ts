4f1f09754b8d2fb9928e46e4459348199ee37b94
vinit717 <vinit224488@gmail.com>
2024-07-31 17:17:00 +0530

    add flow bite library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/flowbite-react/lib/**/*.js',
     ],
     theme: {
         extend: {
@@ -21,6 +22,6 @@ const config: Config = {
             },
         },
     },
-    plugins: [],
+    plugins: [require('flowbite/plugin')],
 };
 export default config;
