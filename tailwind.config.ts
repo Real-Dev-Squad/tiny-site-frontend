e413b40dde30d1c155ab682e831c5b8d8a603876
vinit717 <vinit224488@gmail.com>
2024-08-02 19:17:07 +0530

    Revert "add flow bite library"
    This reverts commit 4f1f09754b8d2fb9928e46e4459348199ee37b94.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-        './node_modules/flowbite-react/lib/**/*.js',
     ],
     theme: {
         extend: {
@@ -22,6 +21,6 @@ const config: Config = {
             },
         },
     },
-    plugins: [require('flowbite/plugin')],
+    plugins: [],
 };
 export default config;
