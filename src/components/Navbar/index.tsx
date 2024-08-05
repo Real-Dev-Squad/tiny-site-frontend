7cda98f38943a062a7bcd78d3e0ac6b7d310485a
vinit717 <vinit224488@gmail.com>
2024-08-05 21:35:55 +0530

    style: fix ui of navbar home dahboard and user profile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,71 +1,62 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
+import { MdOutlineLogout } from 'react-icons/md';
 
 import LoginModal from '@/components/LoginModal';
-import NavbarMenuItems from '@/components/Navbar/NavbarMenuItems';
 import UserProfileButton from '@/components/Navbar/UserProfileButton';
 import UserLoginShimmer from '@/components/ShimmerEffect/UserLoginShimmer';
+import { TINY_API_LOGOUT } from '@/constants/url';
 import useAuthenticated from '@/hooks/useAuthenticated';
 
+import NavbarMenuItems from './NavbarMenuItems';
+
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
     const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+    const [showSignOutButton, setShowSignOutButton] = useState<boolean>(false);
     const { isLoggedIn, isLoading, userData } = useAuthenticated();
 
     const userName = userData?.data?.userName || 'User';
     const [firstName, lastName] = userName.split(' ');
 
-    const handleMenuClick = () => {
-        setMenuOpen(!menuOpen);
+    const handleProfileClick = () => {
+        setShowSignOutButton(!showSignOutButton);
     };
-
-    const renderUserProfile = () => {
-        if (isLoading) {
-            return <UserLoginShimmer />;
-        }
-
-        return (
-            <UserProfileButton
-                isLoggedIn={isLoggedIn}
-                firstName={firstName}
-                lastName={lastName}
-                handleMenuClick={handleMenuClick}
-                setShowLoginModal={setShowLoginModal}
-                isMenuOpen={menuOpen}
-            />
-        );
-    };
-    const navbarRef = useRef<HTMLDivElement>(null);
-
-    const handleOutsideClick = (event: MouseEvent) => {
-        if (navbarRef.current && !navbarRef.current.contains(event.target as Node)) {
-            setMenuOpen(false);
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('mousedown', handleOutsideClick);
-        return () => {
-            document.removeEventListener('mousedown', handleOutsideClick);
-        };
-    }, []);
 
     return (
         <>
-            <nav ref={navbarRef} className=" p-4 h-[8vh]">
+            <nav className="p-4 h-[8vh]">
                 <div className="flex items-center justify-between">
-                    <Link href="/" className="flex">
+                    <Link href="/" className="flex items-center">
                         <Image src="/rds.png" alt="logo" width={30} height={30} className="mr-2 w-30" />
                         <span className="text-white text-2xl font-bold">RDS</span>
                     </Link>
-
-                    <ul className="lg:flex space-x-4">
-                        <li className="relative group ">{renderUserProfile()}</li>
-                        <NavbarMenuItems menuOpen={menuOpen} />
+                    <ul className="flex gap-14 mr-14">
+                        <NavbarMenuItems />
+                        <li className="relative group">
+                            {isLoading ? (
+                                <UserLoginShimmer />
+                            ) : (
+                                <UserProfileButton
+                                    isLoggedIn={isLoggedIn}
+                                    firstName={firstName}
+                                    lastName={lastName}
+                                    handleProfileClick={handleProfileClick}
+                                    setShowLoginModal={setShowLoginModal}
+                                />
+                            )}
+                        </li>
                     </ul>
                 </div>
             </nav>
+            {showSignOutButton && (
+                <div className="absolute top-20 right-10 bg-white p-1 rounded-lg shadow-lg">
+                    <Link href={TINY_API_LOGOUT} className="text-black flex items-center h-8 w-24 text-sm gap-4">
+                        SignOut
+                        <MdOutlineLogout className="h-5 w-5" />
+                    </Link>
+                </div>
+            )}
             {showLoginModal && (
                 <LoginModal onClose={() => setShowLoginModal(false)}>
                     <p className="text-black text-center mb-4">Sign in to your account</p>
