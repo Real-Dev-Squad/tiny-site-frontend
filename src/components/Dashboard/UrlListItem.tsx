1728e64a6b2dca24dac9c369fcf2279165128f5c
vinit717 <vinit224488@gmail.com>
2024-08-13 00:05:32 +0530

    fix user dashboard url list

--- src/components/Dashboard/UrlListItem.tsx
+++ src/components/Dashboard/UrlListItem.tsx
@@ -33,7 +33,7 @@ const UrlListItem = ({ url, copyButtonHandler }: UrlListItemProps) => {
     const { userData } = useAuthenticated();
 
     return (
-        <li className="rounded-lg bg-white w-full px-2 py-2 sm:pl-4 sm:pr-10 sm:py-4 flex flex-col">
+        <li className="rounded-xl bg-white w-full px-2 py-2 sm:pl-4 sm:pr-10 sm:py-4 flex flex-col">
             <div className="flex items-center gap-2 w-full">
                 <div className="flex flex-col w-full">
                     <Link
@@ -53,7 +53,7 @@ const UrlListItem = ({ url, copyButtonHandler }: UrlListItemProps) => {
                         {url.originalUrl}
                     </Link>
                 </div>
-                <div className="flex gap-2 ml-auto">
+                <div className="hidden sm:flex gap-2 ml-auto">
                     <Button
                         className="rounded-full p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 active:scale-95"
                         onClick={() => copyButtonHandler(`${TINY_SITE}/${url.shortUrl}`)}
@@ -83,14 +83,42 @@ const UrlListItem = ({ url, copyButtonHandler }: UrlListItemProps) => {
 
             <hr className="my-2 border-t border-gray-300" />
 
-            <p className="text-sm text-gray-500 flex items-center gap-2">
-                <LiaStopwatchSolid className="text-lg" />
-                {formatDate({
-                    inputDate: url.createdAt as string,
-                    relativeDuration: false,
-                    fullDate: true,
-                })}
-            </p>
+            <div className="flex items-center justify-between">
+                <p className="text-sm text-gray-500 flex items-center gap-2">
+                    <LiaStopwatchSolid className="text-lg" />
+                    {formatDate({
+                        inputDate: url.createdAt as string,
+                        relativeDuration: false,
+                        fullDate: true,
+                    })}
+                </p>
+                <div className="flex sm:hidden gap-2 ml-auto">
+                    <Button
+                        className="rounded-full p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 active:scale-95"
+                        onClick={() => copyButtonHandler(`${TINY_SITE}/${url.shortUrl}`)}
+                        testId="copy-button"
+                    >
+                        <span className="sr-only">Copy</span>
+                        <MdOutlineContentCopy className="text-black" />
+                    </Button>
+
+                    <Button
+                        disabled={deleteMutation.isLoading}
+                        onClick={() => deleteMutation.mutate({ id: url.id, userId: userData?.data?.id })}
+                        className={`flex items-center justify-center w-8 h-8 rounded transition active:scale-95 ${
+                            deleteMutation.isLoading
+                                ? 'text-gray-600 bg-gray-100'
+                                : 'text-red-500 hover:text-red-600 hover:bg-red-100 bg-red-50 sm:bg-transparent'
+                        }`}
+                    >
+                        {!deleteMutation.isLoading ? (
+                            <TbTrash className="w-5 h-5" />
+                        ) : (
+                            <Loader className="w-5 h-5 text-gray-400" />
+                        )}
+                    </Button>
+                </div>
+            </div>
         </li>
     );
 };
