b21b2d33369d6177a8a9c44d85d5f7f61f6d2262
yash raj <yashraj0207@outlook.com>
2024-08-24 03:01:56 +0530

    clean app/index.tsx and InputSection.tsx

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import InputSection from '@/components/App/InputSection';
 import OutputSection from '@/components/App/OutputSection';
 import Layout from '@/components/Layout';
 import LoginModal from '@/components/LoginModal';
 import Modal from '@/components/Modal';
-import { TINY_SITE } from '@/constants/url';
 import useAuthenticated from '@/hooks/useAuthenticated';
 import { useShortenUrlMutation } from '@/services/api';
 import { ErrorResponse } from '@/types/url.types';
@@ -18,52 +17,28 @@ const App = () => {
     const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
     const [showOutputModal, setShowOutputModal] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
-    const [isProcessing, setIsProcessing] = useState<boolean>(false);
 
     const { isLoggedIn, userData } = useAuthenticated();
-    const shortenUrlMutation = useShortenUrlMutation();
 
-    useEffect(() => {
-        const localUrl = localStorage.getItem('url');
-        if (isLoggedIn && localUrl) {
-            setUrl(localUrl);
-            generateShortUrl(localUrl);
-            localStorage.removeItem('url');
-        }
-    }, [isLoggedIn]);
-
-    const generateShortUrl = async (url: string) => {
-        if (!url) {
-            setIsProcessing(false);
-            return;
-        }
-        const validationError = validateUrl(url);
-        if (validationError) {
-            setError(validationError);
-            setIsProcessing(false);
-            return;
-        }
-
-        try {
-            const response = await shortenUrlMutation.mutateAsync({
-                originalUrl: url,
-                userData: userData,
-            });
-            const fullShortUrl = `${TINY_SITE}/${response.shortUrl}`;
-            setShortUrl(fullShortUrl);
-            setShowOutputModal(true);
+    const shortenUrlMutation = useShortenUrlMutation({
+        onSuccess: (res) => {
             setError(null);
-        } catch (e) {
-            const error = e as ErrorResponse;
+            setShowOutputModal(true);
+            setShortUrl(res.shortUrl);
+
+            // clear the url from local storage as it is set when user is not logged in
+            // after the user logs in and generates the short url, we don't need the url anymore
+            localStorage.removeItem('url');
+        },
+        onError: (error: ErrorResponse) => {
             if (error.response && error.response.data && error.response.data.message) {
                 setError(error.response.data.message);
-            } else {
-                setError('An unexpected error occurred');
+                return;
             }
-        } finally {
-            setIsProcessing(false);
+
+            setError('An unexpected error occurred');
         }
-    };
+    });
 
     const createNewHandler = () => {
         setUrl('');
@@ -72,59 +47,68 @@ const App = () => {
         setError(null);
     };
 
-    const handleUrl = () => {
-        if (isProcessing) return;
+    const createShortenedUrl = (originalUrl: string) => {
         if (!isLoggedIn) {
             setShowLoginModal(true);
-            if (url) localStorage.setItem('url', url);
-        } else {
-            setIsProcessing(true);
-            const formattedUrl = formatUrl(url);
-            if (url) {
-                generateShortUrl(formattedUrl);
-            } else {
-                setIsProcessing(false);
-            }
+            localStorage.setItem('url', originalUrl);
+            return;
         }
+
+        const validationError = validateUrl(url);
+
+        if (validationError) {
+            setError(validationError);
+            return;
+        }
+
+        const formattedUrl = formatUrl(url);
+        shortenUrlMutation.mutate({ originalUrl: formattedUrl, userData });
     };
 
     const clearError = () => setError(null);
 
+    useEffect(() => {
+        const localUrl = localStorage.getItem('url');
+        if (!isLoggedIn || !localUrl) return;
+
+        setUrl(localUrl);
+    }, [isLoggedIn]);
+
     return (
-        <Layout title="Home | URL Shortener">
-            <div className="flex justify-center h-[90vh]">
-                <div className="flex flex-col w-[100%]">
-                    <InputSection
-                        url={url}
-                        setUrl={setUrl}
-                        handleUrl={handleUrl}
-                        error={error}
-                        clearError={clearError}
-                    />
-                </div>
-                {showLoginModal && (
-                    <LoginModal onClose={() => setShowLoginModal(false)}>
-                        <p className="text-black text-center mb-4">Log in to generate short links</p>
-                    </LoginModal>
-                )}
-                {showOutputModal && (
-                    <Modal
-                        onClose={() => {
-                            setShowOutputModal(false);
-                            setUrl('');
-                        }}
-                        width="550px"
-                        height="560px"
-                    >
-                        <OutputSection
-                            shortUrl={shortUrl}
-                            isLoaded={!!shortUrl}
-                            originalUrl={url}
-                            handleCreateNew={createNewHandler}
-                        />
-                    </Modal>
-                )}
+        <Layout title="Home | URL Shortener" classNames={{ container: 'h-screen flex flex-col', main: 'flex-1 grid place-items-center' }}>
+            <div className='-mt-44 w-full'>
+                <InputSection
+                    url={url}
+                    error={error}
+                    setUrl={setUrl}
+                    clearError={clearError}
+                    onSubmit={createShortenedUrl}
+                />
             </div>
+
+            {showLoginModal && (
+                <LoginModal onClose={() => setShowLoginModal(false)}>
+                    <p className="text-black text-center mb-4">Log in to generate short links</p>
+                </LoginModal>
+            )}
+
+            {showOutputModal && (
+                <Modal
+                    onClose={() => {
+                        setShowOutputModal(false);
+                        setUrl('');
+                    }}
+                    width="550px"
+                    height="560px"
+                >
+                    <OutputSection
+                        shortUrl={shortUrl}
+                        isLoaded={!!shortUrl}
+                        originalUrl={url}
+                        handleCreateNew={createNewHandler}
+                    />
+                </Modal>
+            )}
         </Layout>
     );
 };
