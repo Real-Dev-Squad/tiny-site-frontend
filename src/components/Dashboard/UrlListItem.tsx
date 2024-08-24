4295d660fb2da7fbfebf21a09446bb31fd12ce5f
vinit717 <vinit224488@gmail.com>
2024-08-24 16:25:45 +0530

    chore: break url list component

--- src/components/Dashboard/UrlListItem.tsx
+++ src/components/Dashboard/UrlListItem.tsx
@@ -1,7 +1,5 @@
 import Link from 'next/link';
 import { LiaStopwatchSolid } from 'react-icons/lia';
-import { MdOutlineContentCopy } from 'react-icons/md';
-import { TbTrash } from 'react-icons/tb';
 import { useMutation } from 'react-query';
 
 import { TINY_SITE } from '@/constants/url';
@@ -11,26 +9,25 @@ import { deleteUrlApi } from '@/services/api';
 import { UrlType } from '@/types/url.types';
 import formatDate from '@/utils/formatDate';
 
-import Button from '../Button';
-import { Loader } from '../Loader';
+import { CopyButton, DeleteButton } from './Buttons';
 
 interface UrlListItemProps {
     url: UrlType;
     copyButtonHandler: (url: string) => void;
 }
-
-const UrlListItem = ({ url, copyButtonHandler }: UrlListItemProps) => {
+const UrlListItem: React.FC<UrlListItemProps> = ({ url, copyButtonHandler }) => {
+    const { userData } = useAuthenticated();
     const deleteMutation = useMutation({
         mutationFn: deleteUrlApi,
-        onSuccess: () => {
-            queryClient.invalidateQueries(['urls']);
-        },
+        onSuccess: () => queryClient.invalidateQueries(['urls']),
         onError: (error) => {
             window.alert('Error deleting URL');
             console.error(error);
         },
     });
-    const { userData } = useAuthenticated();
+
+    const handleCopy = () => copyButtonHandler(`${TINY_SITE}/${url.shortUrl}`);
+    const handleDelete = () => deleteMutation.mutate({ id: url.id, userId: Number(userData?.data?.id) });
 
     return (
         <li className="rounded-xl bg-white px-2 py-2 sm:pl-4 sm:py-4 flex flex-col overflow-hidden">
@@ -54,30 +51,8 @@ const UrlListItem = ({ url, copyButtonHandler }: UrlListItemProps) => {
                     </Link>
                 </div>
                 <div className="hidden sm:flex flex-shrink-0 gap-2 ml-auto">
-                    <Button
-                        className="rounded-full p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 active:scale-95"
-                        onClick={() => copyButtonHandler(`${TINY_SITE}/${url.shortUrl}`)}
-                        testId="copy-button"
-                    >
-                        <span className="sr-only">Copy</span>
-                        <MdOutlineContentCopy className="text-black" />
-                    </Button>
-
-                    <Button
-                        disabled={deleteMutation.isLoading}
-                        onClick={() => deleteMutation.mutate({ id: url.id, userId: userData?.data?.id })}
-                        className={`flex items-center justify-center w-8 h-8 rounded transition active:scale-95 ${
-                            deleteMutation.isLoading
-                                ? 'text-gray-600 bg-gray-100'
-                                : 'text-red-500 hover:text-red-600 hover:bg-red-100'
-                        }`}
-                    >
-                        {!deleteMutation.isLoading ? (
-                            <TbTrash className="w-5 h-5" />
-                        ) : (
-                            <Loader className="w-5 h-5 text-gray-400" />
-                        )}
-                    </Button>
+                    <CopyButton onCopy={handleCopy} />
+                    <DeleteButton isLoading={deleteMutation.isLoading} onDelete={handleDelete} />
                 </div>
             </div>
 
@@ -93,31 +68,8 @@ const UrlListItem = ({ url, copyButtonHandler }: UrlListItemProps) => {
                     })}
                 </p>
                 <div className="flex sm:hidden gap-1 ml-auto">
-                    <Button
-                        className="rounded-full p-1.5 transition-all duration-75 hover:scale-105 hover:bg-blue-100 active:scale-95"
-                        onClick={() => copyButtonHandler(`${TINY_SITE}/${url.shortUrl}`)}
-                        testId="copy-button"
-                    >
-                        <span className="sr-only">Copy</span>
-                        <MdOutlineContentCopy className="text-black" />
-                    </Button>
-
-                    <Button
-                        disabled={deleteMutation.isLoading}
-                        onClick={() => deleteMutation.mutate({ id: url.id, userId: userData?.data?.id })}
-                        className={`flex items-center justify-center w-8 h-8 rounded transition active:scale-95 ${
-                            deleteMutation.isLoading
-                                ? 'text-gray-600 bg-gray-100'
-                                : 'text-red-500 hover:text-red-600 hover:bg-red-100 '
-                        }`}
-                        testId="delete-button"
-                    >
-                        {!deleteMutation.isLoading ? (
-                            <TbTrash className="w-5 h-5" />
-                        ) : (
-                            <Loader className="w-5 h-5 text-gray-400" />
-                        )}
-                    </Button>
+                    <CopyButton onCopy={handleCopy} />
+                    <DeleteButton isLoading={deleteMutation.isLoading} onDelete={handleDelete} />
                 </div>
             </div>
         </li>
