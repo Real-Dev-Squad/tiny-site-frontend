b79383d1c35a9947686ffa709d3fb64e51535e76
vinit717 <vinit224488@gmail.com>
2024-08-31 00:06:52 +0530

    chore: fix navbar alignment and remove discord for sharing

--- src/components/Navbar/DesktopMenu.tsx
+++ src/components/Navbar/DesktopMenu.tsx
@@ -73,27 +73,31 @@ const DesktopMenu: React.FC<DesktopMenuProps> = ({
     const isDashboardPage = router.pathname === '/dashboard';
 
     return (
-        <ul className="hidden sm:flex gap-14 mr-14 items-center">
-            <li className={`relative ${isHomePage ? 'border-b-2 border-white' : ''}`}>
-                <Link href="/" className="text-white hover:text-gray-300">
-                    Home
-                </Link>
-            </li>
-            <li className={`relative ${isDashboardPage ? 'border-b-2 border-white' : ''}`}>
-                <Link href="/dashboard" className="text-white hover:text-gray-300">
-                    Dashboard
-                </Link>
-            </li>
-            <li className="relative group">
-                <ProfileButton
-                    isLoading={isLoading}
-                    isLoggedIn={isLoggedIn}
-                    firstName={firstName}
-                    lastName={lastName}
-                    handleProfileClick={handleProfileClick}
-                    setShowLoginModal={setShowLoginModal}
-                />
-            </li>
+        <ul className="hidden sm:flex justify-between items-center w-full pl-5">
+            <div className="flex gap-14">
+                <li className={`relative ${isHomePage ? 'border-b-2 border-white' : ''}`}>
+                    <Link href="/" className="text-white hover:text-gray-300">
+                        Home
+                    </Link>
+                </li>
+                <li className={`relative ${isDashboardPage ? 'border-b-2 border-white' : ''}`}>
+                    <Link href="/dashboard" className="text-white hover:text-gray-300">
+                        Dashboard
+                    </Link>
+                </li>
+            </div>
+            <div className="ml-auto">
+                <li className="relative group mr-5">
+                    <ProfileButton
+                        isLoading={isLoading}
+                        isLoggedIn={isLoggedIn}
+                        firstName={firstName}
+                        lastName={lastName}
+                        handleProfileClick={handleProfileClick}
+                        setShowLoginModal={setShowLoginModal}
+                    />
+                </li>
+            </div>
         </ul>
     );
 };
