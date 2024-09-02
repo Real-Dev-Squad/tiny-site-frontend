1ee17cce0444c83088e45193fae1cfb0761acc0c
vinit717 <vinit224488@gmail.com>
2024-09-03 00:45:58 +0530

    chore: change tinysite to tiny

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://staging-tinysite-api.realdevsquad.com/v1';
-export const TINY_SITE = process.env.NEXT_PUBLIC_TINY_SITE || 'https://staging-tinysite.realdevsquad.com';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://services.realdevsquad.com/staging-tiny/v1';
+export const TINY_SITE = process.env.NEXT_PUBLIC_TINY_SITE || 'https://staging-tiny.realdevsquad.com';
 
 export const TINY_API_URL = API_BASE_URL;
 export const TINY_API_GOOGLE_LOGIN = `${API_BASE_URL}/auth/google/login`;
